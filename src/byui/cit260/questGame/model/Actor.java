86b52eddadb9277b48d197dd92e27908889a88c7
Harry <Harry@10.18.183.28>
2017-01-28 19:25:42 -0700

    4 new classes and their calling in Main

---
+++ src/byui/cit260/questGame/model/Actor.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.questGame.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Harry
+ */
+public class Actor implements Serializable{
+    private String name;
+    private String description;
+
+    
+    
+    public Actor() {
+    }
+
+    public Actor(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+  
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + Objects.hashCode(this.name);
+        hash = 59 * hash + Objects.hashCode(this.description);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Actor other = (Actor) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" + "name=" + name + ", description=" + description + '}';
+    }
+    
+    
+    
+    
+}
--- src/quest/Quest.java
+++ src/quest/Quest.java
@@ -6,6 +6,11 @@
 package quest;
 
 import byui.cit260.questGame.model.Player;
+import byui.cit260.questGame.model.Location;
+import byui.cit260.questGame.model.Actor;
+import byui.cit260.questGame.model.Scene;
+import byui.cit260.questGame.model.Map;
+
 
 /**
  *
@@ -19,13 +24,27 @@ public class Quest {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        /*******************************************/
         Player p1 = new Player();
-        p1.setName("Ashley");
-        
-        
+        p1.setName("Harry"); 
         String playerValue = p1.toString();
-        
         System.out.println(playerValue);
+        
+        /*******************************************/
+        Actor a1 = new Actor("Harry", "Student in CIT 260");
+        System.out.println(a1.toString());
+        
+        /*******************************************/
+        Location l1 = new Location(1,1,true);
+        System.out.println(l1.toString());
+        
+        /*******************************************/
+        Map m1 = new Map(6,6);
+        System.out.println(m1.toString());
+        
+        /*******************************************/
+        Scene s1 = new Scene("Austin Level 1", 1, false);   
+        System.out.println(s1.toString());
         
     }
     
