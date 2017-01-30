895d376b5bc180a169a5d280d480031275ff19b9
Morley <Morley@Morley-PC.Home>
2017-01-29 23:55:56 -0700

    1/29/17 - initial commit for Riddle and DiplomaPiece class

---
+++ src/byui/cit260/questGame/model/Riddle.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.questGame.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Morley
+ */
+public class Riddle implements Serializable{
+    
+        //class instance variables
+        private String name;
+        private double bestTime;
+
+    public Riddle() {
+    }
+        
+        
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.name);
+        hash = 17 * hash + (int) (Double.doubleToLongBits(this.bestTime) ^ (Double.doubleToLongBits(this.bestTime) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Riddle{" + "name=" + name + ", bestTime=" + bestTime + '}';
+    }
+
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Riddle other = (Riddle) obj;
+        if (Double.doubleToLongBits(this.bestTime) != Double.doubleToLongBits(other.bestTime)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+    
+
+    public double getBestTime() {
+        return bestTime;
+    }
+
+    public void setBestTime(double bestTime) {
+        this.bestTime = bestTime;
+    }
+        
+        
+}
---
+++ src/thequest/TheQuest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thequest;
+
+import byui.cit260.questGame.model.DiplomaPiece;
+import byui.cit260.questGame.model.Riddle;
+
+/**
+ *
+ * @author Morley
+ */
+public class TheQuest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // Riddle riddleOne = new Riddle();
+        DiplomaPiece diplomaPieceOne = new DiplomaPiece();
+        
+       // riddleOne.setName("Fred Flinstone");
+        //riddleOne.setBestTime(7.00);
+        
+        diplomaPieceOne.setName("Barney Rubble");
+        diplomaPieceOne.setBestTime(7.00);
+        
+       // String playerInfo = riddleOne.toString();
+        //System.out.println(playerInfo);
+        
+        String playerInfo = diplomaPieceOne.toString();
+        System.out.println(playerInfo);
+        
+        
+    }
+    
+}
