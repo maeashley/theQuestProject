db90a1acb6f0cc2df770dab3c036a34c2026bd53
maeashley <ashleystewart8888@yahoo.com>
2017-02-01 17:49:15 -0700

    major changes.
    Added to the main function to test backpack and game classes

--- src/thequest/TheQuest.java
+++ src/thequest/TheQuest.java
@@ -7,6 +7,13 @@ package thequest;
 
 import byui.cit260.questGame.model.DiplomaPiece;
 import byui.cit260.questGame.model.Riddle;
+import byui.cit260.questGame.model.Player;
+import byui.cit260.questGame.model.Location;
+import byui.cit260.questGame.model.Actor;
+import byui.cit260.questGame.model.Scene;
+import byui.cit260.questGame.model.Map;
+import byui.cit260.questGame.model.Backpack;
+import byui.cit260.questGame.model.Game;
 
 /**
  *
@@ -32,6 +39,37 @@ public class TheQuest {
         
         String playerInfo = diplomaPieceOne.toString();
         System.out.println(playerInfo);
+         /*******************************************/
+        Player p1 = new Player();
+        p1.setName("Harry"); 
+        String playerValue = p1.toString();
+        System.out.println(playerValue);
+        
+        /*******************************************/
+        Actor a1 = new Actor("Harry", "Student in CIT 260");
+        System.out.println(a1.toString());
+        
+        /*******************************************/
+        Location l1 = new Location(1,1,true);
+        System.out.println(l1.toString());
+        
+        /*******************************************/
+        Map m1 = new Map(6,6);
+        System.out.println(m1.toString());
+        
+        /*******************************************/
+        Scene s1 = new Scene("Austin Level 1", 1, false);   
+        System.out.println(s1.toString());
+        
+        //test Game
+        Game startGame = new Game(2);
+        
+        startGame.setNoPeople(2);
+        
+        String start= startGame.toString();
+        System.out.println(start);
+        
+        //test Backpack
         
         
     }
