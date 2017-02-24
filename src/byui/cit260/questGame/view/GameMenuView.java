2a81b75ee1d2260b2b83fadc5cc6b1c215a36821
Harry <Harry@10.49.121.80>
2017-02-23 19:27:48 -0700

    GameMeneViewChanges

--- src/byui/cit260/questGame/view/GameMenuView.java
+++ src/byui/cit260/questGame/view/GameMenuView.java
@@ -5,14 +5,112 @@
  */
 package byui.cit260.questGame.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Harry
  */
 public class GameMenuView {
 
-    void displayMenu() {
-       System.out.println("DisplayMenu");
+    private String menu;
+    private String promptMessage;
+
+    public GameMenuView() {
+        this.menu = "V – View Map\n"
+                + "B - BackPack Contents   \n"
+                + "L – Current Location\n"
+                + "H - Help \n"
+                + "Q – return to previous menu";
+        
+        
+        this.promptMessage = "\nEnter the option: ";
     }
+
+    private String getGameOption() {
+        
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid = false;
+        
+        while (!valid){
+            System.out.println(this.menu);
+            System.out.println("\n" + this.promptMessage);
+            value = keyboard.nextLine();
+            value = value.trim();
+            
+            if (value.length()<1){
+            System.out.println("\nInvalid value: value cannot be blank");
+            continue;
+            }
+            break;
+        }
+        return value;
+    }
+    void displayGameMenuView() {
+        boolean done = false;
+        do {
+            String gameOption = this.getGameOption();
+            if (gameOption.toUpperCase().equals("Q")) {
+                return;
+            }
+
+            done = this.doAction(gameOption);
+        } while (!done);
+    }
+
+    private boolean doAction(String choice) {
+        choice = choice.toUpperCase();
+
+        switch (choice) {
+            case "V":
+                this.map();
+                break;
+
+            case "B":
+                this.backPack();
+                break;
+
+            case "L":
+                this.location();
+                break;
+
+            case "H":
+                this.help();
+                break;
+            
+            default:
+                System.out.println("\n***Invalid selection *** Try again");
+                break;
+        }
+        return false;
+    }
+
+    private void backPack() {
+       System.out.println("BackPack Option takes you to the BackPack Menu");
+    }
+
+    private void location() {
+       System.out.println("Location takes you the Menu for your current Location"
+               + "Provides options to solve riddle and Helps options in the BackPack");
+    }
+
+    private void help() {
+       HelpMenuView helpMenu = new HelpMenuView();
+        helpMenu.displayHelpMenuView();
+    }
+
+    private void map() {
+       System.out.println("Map");
+    }
+
+    void displayMenu() {
+        
+        System.out.println(menu);
+         System.out.println(promptMessage);
+    
+    }
+    
+
     
 }
