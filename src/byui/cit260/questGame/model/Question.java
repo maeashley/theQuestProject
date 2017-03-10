981e70e74ae4769b3093c500caab9034cc9cfe4b
caleb <caleb@caleb-tablet>
2017-03-09 20:26:25 -0700

    populated the Qquestion class with an array

--- src/byui/cit260/questGame/model/Question.java
+++ src/byui/cit260/questGame/model/Question.java
@@ -39,11 +39,76 @@ public class Question {
         Question[] religion = new Question[10];
         
         Question zero = new Question();
-     
+        zero.setText("What is the first book in the Book of Mormon?");
+        zero.setCorrectAnswer("Nephi");
+        zero.setReward(1);
+        
+        
+        Question one = new Question();
+        one.setText("How many years were Nephi andhis family in the wilderness?");
+        one.setCorrectAnswer("8");
+        one.setReward(1);
+        
+        Question two = new Question();
+        two.setText("What does the iron rod symoblize?");
+        two.setCorrectAnswer("the word of God");
+        two.setReward(1);
+        
+        Question three = new Question();
+        three.setText("Which Book of Mormon character was famous for"
+                + " preaching the gospel on a large wall");
+        three.setCorrectAnswer("Samuel the Lamanite");
+        three.setReward(1);
+        
+        Question four = new Question();
+        four.setText("Which King did Abinadi preach to?");
+        four.setCorrectAnswer("Noah");
+        four.setReward(1);
+        
+        Question five = new Question();
+        five.setText("How many books are there in the Book of Mormon?");
+        five.setCorrectAnswer("13");
+        five.setReward(2);
+        
+        Question six = new Question();
+        six.setText("What guided Lehi's family to the promised land?");
+        six.setCorrectAnswer("Liahona");
+        six.setReward(2);
+        
+        Question seven = new Question();
+        seven.setText("Which prophet laid the brass plates in Hill Cumorah?");
+        seven.setCorrectAnswer("Moroni");
+        seven.setReward(2);
+        
+        Question eight= new Question();
+        eight.setText("Which prophet (full name) saw the finger of the Lord"
+                + " touch stones and give them light?");
+        eight.setCorrectAnswer("Mahonrimoriancumr");
+        eight.setReward(3);
+        
+        Question nine = new Question();
+        nine.setText("Fill in the blank: \n ... and by the power of the Holy"
+                + " Ghost, ye may know the ______ of all things.");
+        nine.setCorrectAnswer("truth");
+        nine.setReward(1);
     
         
         
         return religion;
+    }
+
+    
+    
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public void setReward(int reward) {
+        this.reward = reward;
     }
 
     
